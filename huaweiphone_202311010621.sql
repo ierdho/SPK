583fec2ed0faa6cae049084e7a8a7a28ded1dffd
ierdho <120251400+ierdho@users.noreply.github.com>
2023-11-01 06:46:45 +0700

    Add files via upload

---
+++ huaweiphone_202311010621.sql
@@ -0,0 +1,11 @@
+INSERT INTO huaweiphone (nama_produk,layar,prosesor,ram,penyimpanan,kamera_belakang,kamera_depan,baterai,harga) VALUES
+	 ('Huawei Y5p','5.45 inci, IPS LCD, 720 x 1440 piksel','MediaTek MT6762R 2.0 GHZ',2,32,8,5,3020,'Rp1.499.000'),
+	 ('Huawei Y6p','6.3 inci, IPS LCD, 720 x 1600 piksel','MediaTek MT6762R 2.0 GHz',3,64,13,8,5000,'Rp1.999.000'),
+	 ('Huawei Y7p','6.39 inci, IPS LCD, 720 x 1560 piksel','HiSilicon Kirin 710F 2.2 GHz',4,64,48,8,4000,'Rp2.499.000'),
+	 ('Huawei Y8p','6.3 inci, OLED, 1080 x 2400 piksel','HiSilicon Kirin 710F 2.2 GHz',6,128,48,16,4000,'Rp3.499.000'),
+	 ('Huawei Y9s','6.59 inci, LTPS IPS LCD, 1080 x 2340 piksel','HiSilicon Kirin 710F2.2 GHz',6,128,48,16,4000,'Rp3.999.000'),
+	 ('Huawei P40 Lite E','6.39 inci, IPS LCD, 720 x 1560 piksel','HiSilicon Kirin 710F 2.2 GHz',4,64,48,8,4000,'Rp2.799.000'),
+	 ('Huawei P Smart 2021','6.67 inci, IPS LCD, 1080 x 2400 piksel','HiSilicon Kirin 710A 2.0 GHz',4,128,48,8,5000,'Rp3.299.000'),
+	 ('Huawei Nova 7i','6.4 inci, LTPS IPS LCD, 1080 x 2310 piksel','HiSilicon Kirin 810 2.27 GHz',8,128,48,16,4200,'Rp4.999.000'),
+	 ('Huawei Enjoy 10e','6.3 inci, IPS LCD, 720 x 1600 piksel','MediaTek Helio P35 2.3 GHz',4,64,13,8,5000,'Rp2.299.000'),
+	 ('Huawei Mate 30 Lite','6.26 inci, LTPS IPS LCD, 1080 x 2340 piksel','HiSilicon Kirin 810 2.27 GHz',6,128,48,24,4000,'Rp5.999.000');
